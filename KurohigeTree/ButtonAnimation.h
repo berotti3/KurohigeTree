b1b6485338d77e925466a21d38dd297acb5d3524
Sayuri Anbe <bero.sa.91@gmail.com>
2014-06-06 19:06:45 +0900

    first commit

---
+++ KurohigeTree/ButtonAnimation.h
@@ -0,0 +1,13 @@
+//
+//  ButtonAnimation.h
+//  KurohigeTree
+//
+//  Created by Sayuri Anbe on 13/02/08.
+//  Copyright (c) 2013年 Sayuri Anbe. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface ButtonAnimation : CAAnimation
+
+@end
---
+++ KurohigeTree/ButtonAnimation.m
@@ -0,0 +1,13 @@
+//
+//  ButtonAnimation.m
+//  KurohigeTree
+//
+//  Created by Sayuri Anbe on 13/02/08.
+//  Copyright (c) 2013年 Sayuri Anbe. All rights reserved.
+//
+
+#import "ButtonAnimation.h"
+
+@implementation ButtonAnimation
+
+@end
